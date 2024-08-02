f0fa25235630f17dfe4d428750caee46b91f7670
Osman Pérez <operez@edutecno.com>
2024-08-01 21:45:39 -0400

    Operador condicional AND

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    var deportistas = []
     $("#formulario").submit(function(even) {
         even.preventDefault();
         $("#msg-n1, #msg-n2").html("")
@@ -33,4 +35,59 @@ $(document).ready(function() {
             $("#result").html(`Proceso fallido`)
         }
     })
+
+    $("#formulario-40").submit(function(even) {
+        even.preventDefault();
+        $("#msg-rut, #msg-edad, #msg-estatura, #msg-peso").html("")
+
+        var rut = $("#txt-rut").val();
+        var edad = $("#txt-edad").val();
+        var estatura = $("#txt-estatura").val();
+        var peso = $("#txt-peso").val();
+
+        if(rut == "") {
+            return $("#msg-rut").html("Ingresar el RUT")
+        }
+
+        if(edad == "") {
+            return $("#msg-edad").html("Ingresar la edad")
+        }
+
+        if(estatura == "") {
+            return $("#msg-estatura").html("Ingresar la estatura")
+        }
+
+        if(peso == "") {
+            return $("#msg-peso").html("Ingresar el peso")
+        }
+
+        var deportista = {
+            rut: rut,
+            edad: edad,
+            estatura: estatura,
+            peso: peso,
+            admitido: (edad <= 18 && estatura >= 180 && peso <= 80)
+        }
+
+        deportistas.push(deportista)
+        $("#txt-rut, #txt-edad, #txt-estatura, #txt-peso").val("")
+        listarDeportistas(deportistas)
+    })
+
+    function listarDeportistas(deportistas) {
+        $("#listado-deportistas tbody").html("");
+        for (const deportista of deportistas) {
+            $("#listado-deportistas tbody").append(`
+                <tr>
+                    <td>${deportista.rut}</td>
+                    <td>${deportista.edad}</td>
+                    <td>${deportista.estatura}</td>
+                    <td>${deportista.peso}</td>
+                    <td class="${deportista.admitido ? "bg-success-subtle" : "bg-danger-subtle"}">
+                        ${deportista.admitido ? "Admitido" : "No Admitido"}
+                    </td>
+                </tr>
+            `)
+        }
+    }
 })
